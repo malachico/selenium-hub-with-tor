14783eb7c48c8041a6b8614be4a57b2f16d97e71
malachico <malachicohen@gmail.com>
2016-12-11 11:11:44 +0200

    removed bad sites

--- traffic_generator.py
+++ traffic_generator.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-NUM_OF_CONTAINERS = 1  # 10
+NUM_OF_CONTAINERS = 5  # 10
 
 print "starting ... "
 GRID_URL = 'http://172.17.0.2:4444/wd/hub'
@@ -12,35 +12,29 @@ tor_profile.set_preference('network.proxy.type', 1)
 tor_profile.set_preference('network.proxy.socks', '127.0.0.1')
 tor_profile.set_preference('network.proxy.socks_port', 9050)
 
-try:
-    print "configuring chrome driver... "
-    chrome_driver = webdriver.Remote(command_executor=GRID_URL,
-                                     desired_capabilities=DesiredCapabilities.CHROME)
+print "configuring chrome driver... "
+chrome_driver = webdriver.Remote(command_executor=GRID_URL,
+                                 desired_capabilities=DesiredCapabilities.CHROME)
 
-    # print "configuring firefox driver... "
-    # firefox_driver = webdriver.Remote(command_executor=GRID_URL,
-    #                                   desired_capabilities=DesiredCapabilities.FIREFOX)
+print "configuring tor driver... "
+tor_driver = webdriver.Remote(command_executor=GRID_URL,
+                              desired_capabilities=DesiredCapabilities.FIREFOX, browser_profile=tor_profile)
 
-    print "configuring tor driver... "
-    tor_driver = webdriver.Remote(command_executor=GRID_URL,
-                                  desired_capabilities=DesiredCapabilities.FIREFOX, browser_profile=tor_profile)
+with open('sites.txt') as f:
+    for line in f:
+        try:
+            print "tor getting", line
+            tor_driver.get(line)
 
-    with open('sites.txt') as f:
-        for line in f:
             for i in range(NUM_OF_CONTAINERS):
-                print "chrome getting", line
+                print i, "chrome getting", line
                 chrome_driver.get(line)
+        except:
+            print "exception occured at", line
 
-                # print "firefox getting", line
-                # firefox_driver.get(line)
 
-                print "tor getting", line
-                tor_driver.get(line)
-
-finally:
-    print "closing windows ... "
-    # chrome_driver.quit()
-    # firefox_driver.quit()
-    tor_driver.quit()
+print "closing windows ... "
+chrome_driver.quit()
+tor_driver.quit()
 
 print "SUCCESS"
